bfb01c5c8287ef1c1585a1c9c66220bade4d0eee
runner-jaimie <jlee723@myseneca.ca>
2025-10-07 14:01:30 -0400

    feat(ssg): migrate home page rendering from SSR to SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,32 @@
 import SearchableLayout from '@/components/searchable-layout';
 import style from './index.module.css';
 import { ReactNode } from 'react';
-import books from '@/mock/books.json';
 import BookItem from '@/components/book-item';
-import { InferGetServerSidePropsType } from 'next';
+import { InferGetStaticPropsType } from 'next';
 import fetchBooks from '@/lib/fetch-books';
 import fetchRandomBooks from '@/lib/fetch-random-books';
 
-export const getServerSideProps = async () => {
-  // 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터 불러오는 함수
-  // const allBooks = await fetchBooks();  // 이 두함수는 직렬로 함수가 실행 즉 모든 도서를 불러온 후 추천 도서를 불러오기 때문에
-  // const recoBooks = await fetchRandomBooks();
+// 서버사이드 렌더링 (SSR) - 매 요청마다 서버에서 데이터를 불러와서 렌더링
+// export const getServerSideProps = async () => {
+//   // 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터 불러오는 함수
+//   // const allBooks = await fetchBooks();  // 이 두함수는 직렬로 함수가 실행 즉 모든 도서를 불러온 후 추천 도서를 불러오기 때문에
+//   // const recoBooks = await fetchRandomBooks();
 
-  // 위의 코드를 아래처럼 바꿔주면 두 함수가 병렬로 실행됨 (동시에 2개의 api가 실행됨)
+//   // 위의 코드를 아래처럼 바꿔주면 두 함수가 병렬로 실행됨 (동시에 2개의 api가 실행됨)
+//   const [allBooks, recoBooks] = await Promise.all([
+//     fetchBooks(),
+//     fetchRandomBooks(),
+//   ]);
+
+//   return {
+//     props: { allBooks, recoBooks }, // 컴포넌트에 전달할 데이터
+//   };
+// };
+
+// SSG로 변경 (정적 사이트 생성) - 빌드 시점에 데이터를 불러와서 정적으로 생성
+export const getStaticProps = async () => {
+  console.log('getStaticProps 실행됨');
+
   const [allBooks, recoBooks] = await Promise.all([
     fetchBooks(),
     fetchRandomBooks(),
@@ -22,11 +36,10 @@ export const getServerSideProps = async () => {
     props: { allBooks, recoBooks }, // 컴포넌트에 전달할 데이터
   };
 };
-
 export default function Home({
   allBooks,
   recoBooks,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <div className={style.container}>
       <section>
