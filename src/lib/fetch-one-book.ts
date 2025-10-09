7e8b81f18b83cbb422e1d4068b1d44577d58bfab
runner-jaimie <jlee723@myseneca.ca>
2025-10-09 09:45:13 -0400

    chore(fetch): update API endpoints after server deployment

--- src/lib/fetch-one-book.ts
+++ src/lib/fetch-one-book.ts
@@ -1,7 +1,7 @@
 import { BookData } from '@/types';
 
 export default async function fetchBook(id: number): Promise<BookData | null> {
-  const url = `http://localhost:12345/book/${id}`;
+  const url = `https://onebite-books-server-main-six-gamma.vercel.app/book/${id}`;
 
   try {
     const response = await fetch(url);
